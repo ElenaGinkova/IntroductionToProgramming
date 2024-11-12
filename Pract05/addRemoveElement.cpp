e18d38a325ffb9edb842273c8ce6d33e6006f478
ivayloknchv <46057611+ivayloknchv@users.noreply.github.com>
2024-11-12 13:52:55 +0200

    Create addRemoveElement.cpp

---
+++ Pract05/addRemoveElement.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+void removeElement(int arr[], int& size, int pos)
+{
+	for (int i = pos; i < size - 1; i++)
+	{
+		arr[i] = arr[i + 1];
+	}
+
+	arr[size - 1] = 0;
+
+	--size;
+}
+
+void addElement(int arr[], int& size, int pos, int el)
+{
+	for (int i = size; i >= pos; i--)
+	{
+		arr[i] = arr[i - 1];
+	}
+
+	arr[pos] = el;
+	++size;
+}
+
+void print(const int arr[], int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		std::cout << arr[i] << ' ';
+	}
+	std::cout << '\n';
+}
+
+int main()
+{
+	const int MAX_SIZE = 100;
+	int arr[MAX_SIZE] = { 1,2,3,4,5 };
+	int size = 5;
+	print(arr, size);
+
+	addElement(arr, size, 1, 100);
+	print(arr, size);
+	return 0;
+}
