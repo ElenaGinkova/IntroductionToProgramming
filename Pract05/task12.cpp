2218948f0bc20b19aabd249e5be607b87a613483
Elena Ginkova <99428447+ElenaGinkova@users.noreply.github.com>
2024-11-16 11:01:47 +0200

    fixed main

--- Pract05/task12.cpp
+++ Pract05/task12.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
+const int MAX_NUM = 20;
+const int MAX_SIZE = 100;
+
 bool areAllSymbUnique(const int arr[], int size) // int* arr 
 {
     bool histogram[MAX_NUM]{ false };//[0,1,..,20]
     for (int i = 0; i < size; i++)
     {
         int index = arr[i];//the number corresponds to the index
-        if(histogram[index])//we have already seen the number
+        if (histogram[index])//we have already seen the number
         {
             return false;
         }
@@ -16,8 +19,8 @@ bool areAllSymbUnique(const int arr[], int size) // int* arr
 
 int main()
 {
-    int arr[] = {1, 2, 2};
-    int arr2[] = { 1,2,3 };
-    std::cout << areAllSymbUnique(arr, 3);
+    int arr[MAX_SIZE] = { 1, 2, 2 };
+    int arr2[MAX_SIZE] = { 1, 2, 3 };
+    std::cout << areAllSymbUnique(arr, 3) << std::endl;
     std::cout << areAllSymbUnique(arr2, 3);
 }
