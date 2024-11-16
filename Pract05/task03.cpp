f6e569bb9273480d3bc119053648a935eb62147b
Elena Ginkova <99428447+ElenaGinkova@users.noreply.github.com>
2024-11-16 10:56:38 +0200

    Added main()

--- Pract05/task03.cpp
+++ Pract05/task03.cpp
@@ -30,3 +30,36 @@ void productArrays(const int arr1[], int size1, const int arr2[], int size2, int
     }
 
 }
+
+
+int findMax(const int a, const int b)
+{
+    return a > b ? a : b;
+}
+
+int main()
+{
+    const int MAX_CAPACITY = 100;
+    int arr1Size = 0;
+    int arr2Size = 0;
+    int arr1[MAX_CAPACITY]{};
+    int arr2[MAX_CAPACITY]{};
+    int arr3[MAX_CAPACITY]{};
+    std::cin >> arr1Size;
+    for (int i = 0; i < arr1Size; i++)
+    {
+        std::cin >> arr1[i];
+    }
+    std::cin >> arr2Size;
+    for (int i = 0; i < arr2Size; i++)
+    {
+        std::cin >> arr2[i];
+    }
+
+    productArrays(arr1, arr1Size, arr2, arr2Size, arr3);
+    for (int i = 0; i < findMax(arr1Size, arr2Size); i++)
+    {
+        std::cout << arr3[i] << ' ';
+    }
+
+}
